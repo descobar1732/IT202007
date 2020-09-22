c1f3331fae453ebe240d690e94ace66487b46caa
DanielEscobar <de83@njit.edu>
2020-09-21 21:43:14 -0400

    added comments

--- phploops.php
+++ phploops.php
@@ -6,4 +6,7 @@ foreach ($nums as $num){
     }
 
 }
+
+# The loop goes through every number and checks to see if the remainder of that number when divided by two is zero to make sure it is even 
+# If a numbers remainder  is zero  the number is printed as it is an even number
 ?>
