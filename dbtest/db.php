98d235dd12d61a16d01e39a33a8e5c020d9a6820
DanielEscobar <de83@njit.edu>
2020-10-21 18:46:51 -0400

    adding styles

--- dbtest/db.php
+++ dbtest/db.php
@@ -1,4 +1,4 @@
-C:\Users\night\OneDrive\Documents\NJIT\3rd Year\repo\lib\C:\Users\night\OneDrive\Documents\NJIT\3rd Year\repo\lib\<?php
+<?php
 //for this we'll turn on error output so we can try to see any problems on the screen
 //this will be active for any script that includes/requires this one
 ini_set('display_errors', 1);
@@ -12,7 +12,7 @@ function getDB(){
         try{
             //__DIR__ helps get the correct path regardless of where the file is being called from
             //it gets the absolute path to this file, then we append the relative url (so up a directory and inside lib)
-            require_once(__DIR__. "/afs/cad.njit.edu/u/d/e/de83/public_html/IT202007/lib/config.php");//pull in our credentials
+            require_once(__DIR__ . "/../lib/config.php");//pull in our credentials
             //use the variables from config to populate our connection
             $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
             //using the PDO connector create a new connect to the DB
